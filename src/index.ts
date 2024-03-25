e87e9d74641913079cc5cc586002ac14583c33a7
Steve Jenkins <github@skysteve.com>
2024-03-25 16:54:45 -0700

    update url

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,50 @@
-
-import * as components from './components';
-import {NavBar} from './components/NavBar';
-import { EventTypes } from './interfaces/Events';
-import { UserDataLoadedEvent } from './events/UserDataLoadedEvent';
-import { UserSigninRequiredEvent } from './events/UserSigninRequiredEvent';
-import { SectionElement } from './components/ui-elements/SectionElement';
+import * as components from "./components";
+import { NavBar } from "./components/NavBar";
+import { EventTypes } from "./interfaces/Events";
+import { UserDataLoadedEvent } from "./events/UserDataLoadedEvent";
+import { UserSigninRequiredEvent } from "./events/UserSigninRequiredEvent";
+import { SectionElement } from "./components/ui-elements/SectionElement";
 
 // we need to pass in components to make TS/rollup happy the files are being used
 function init(componentList: any) {
-  const elContainer = document.getElementById('main-content') as HTMLDivElement;
+  const elContainer = document.getElementById("main-content") as HTMLDivElement;
 
   window.addEventListener(EventTypes.userData, (event: UserDataLoadedEvent) => {
-    elContainer.innerHTML = '<section-seed-list></section-seed-list><section-knobs-panel collapsed></section-knobs-panel><section-recommendations-list collapsed></section-recommendations-list>';
+    elContainer.innerHTML =
+      "<section-seed-list></section-seed-list><section-knobs-panel collapsed></section-knobs-panel><section-recommendations-list collapsed></section-recommendations-list>";
   });
 
-  window.addEventListener(EventTypes.userSigninRequired, (event: UserSigninRequiredEvent) => {
-    elContainer.innerHTML = '<h1>TODO - welcome, please sign in</h1>';
-  });
+  window.addEventListener(
+    EventTypes.userSigninRequired,
+    (event: UserSigninRequiredEvent) => {
+      elContainer.innerHTML = "<h1>Welcome, please sign in</h1>";
+    }
+  );
 
   window.addEventListener(EventTypes.allSeedsSelectedEvent, () => {
-    const elSeedList = elContainer.querySelector('section-seed-list') as SectionElement;
-    const elKnobsPanel = elContainer.querySelector('section-knobs-panel') as SectionElement;
+    const elSeedList = elContainer.querySelector(
+      "section-seed-list"
+    ) as SectionElement;
+    const elKnobsPanel = elContainer.querySelector(
+      "section-knobs-panel"
+    ) as SectionElement;
     elKnobsPanel.toggleCollapse();
     elSeedList.collapseSection();
   });
-
 
   // this just stop TS complaining about unused vars
   return new NavBar();
 }
 
 // if the document has already loaded - just call init, otherwise wait until it's ready
-if (document.readyState === 'interactive' || document.readyState === 'complete') {
+if (
+  document.readyState === "interactive" ||
+  document.readyState === "complete"
+) {
   init(components);
 } else {
-  document.addEventListener('readystatechange', () => {
-    if (document.readyState === 'interactive') {
+  document.addEventListener("readystatechange", () => {
+    if (document.readyState === "interactive") {
       init(components);
     }
   });
--- src/workers/spotifyApiHelper/ApiHelper.ts
+++ src/workers/spotifyApiHelper/ApiHelper.ts
@@ -1,21 +1,21 @@
-import { IPlaylistData, MessageType } from '../../interfaces/WorkerMessage';
-import { ISpotifyUser } from '../../interfaces/spotify/SpotifyUser';
-import { IAuthTokens } from '../../interfaces/AuthTokens';
+import { IPlaylistData, MessageType } from "../../interfaces/WorkerMessage";
+import { ISpotifyUser } from "../../interfaces/spotify/SpotifyUser";
+import { IAuthTokens } from "../../interfaces/AuthTokens";
 
-const ROOT_URL = 'https://api.spotify.com/v1';
-const AUTH_URL = 'http://localhost:3847'; // 'https://iskpng28a8.execute-api.eu-west-1.amazonaws.com/Prod'; //'http://localhost:3000';
+const ROOT_URL = "https://api.spotify.com/v1";
+const AUTH_URL = "https://iskpng28a8.execute-api.eu-west-1.amazonaws.com/Prod"; //'http://localhost:3000'; //'http://localhost:3847'; //
 
 enum REQUEST_TYPE {
-  GET = 'get',
-  POST = 'post',
-  PUT = 'put'
+  GET = "get",
+  POST = "post",
+  PUT = "put"
 }
 
 export class ApiHelper {
-  private accessToken: string | void = '';
-  private refreshToken: string | void = '';
+  private accessToken: string | void = "";
+  private refreshToken: string | void = "";
   private expiresAt: number;
-  private country: string = '';
+  private country: string = "";
 
   constructor() {
     const params = this.getHashParams();
@@ -39,14 +39,23 @@ export class ApiHelper {
   }
 
   public async getUserData(): Promise<ISpotifyUser> {
-    const userData = await this.makeRequest('me', REQUEST_TYPE.GET) as ISpotifyUser;
+    const userData = (await this.makeRequest(
+      "me",
+      REQUEST_TYPE.GET
+    )) as ISpotifyUser;
     // save the user's country
     this.country = userData.country;
 
     return userData;
   }
 
-  public async search(query: string, type: string, market?: string, limit = 10, offset = 0) {
+  public async search(
+    query: string,
+    type: string,
+    market?: string,
+    limit = 10,
+    offset = 0
+  ) {
     // if there's no market - use the user's country
     if (!market) {
       market = this.country;
@@ -58,25 +67,45 @@ export class ApiHelper {
       throw new Error(`Not Signed in - TODO send a message to login`);
     }
 
-    const queryString = `q=${escape(query.replace(/\s/g, '+'))}&type=${type}&market=${market}&limit=${limit}&offset=${offset}`;
+    const queryString = `q=${escape(
+      query.replace(/\s/g, "+")
+    )}&type=${type}&market=${market}&limit=${limit}&offset=${offset}`;
 
     return await this.makeRequest(`search?${queryString}`);
   }
 
-  public async loadRecommendations(artists: any[] = [], knobs?: any, limit: number = 25) {
+  public async loadRecommendations(
+    artists: any[] = [],
+    knobs?: any,
+    limit: number = 25
+  ) {
     const seedArtists = artists.map(artist => artist.id);
-    const knobsToQS = !knobs ? null : Object.keys(knobs).map(k => {
-      // duration is a bit different - we need to multiply the seconds to ms
-      if (k === 'duration') {
-        return `min_duration_ms=${(knobs[k].min*1000).toString()}&max_duration_ms=${(knobs[k].max*1000).toString()}`;
-      }
+    const knobsToQS = !knobs
+      ? null
+      : Object.keys(knobs)
+          .map(k => {
+            // duration is a bit different - we need to multiply the seconds to ms
+            if (k === "duration") {
+              return `min_duration_ms=${(
+                knobs[k].min * 1000
+              ).toString()}&max_duration_ms=${(
+                knobs[k].max * 1000
+              ).toString()}`;
+            }
 
-      return [
-        `min_${encodeURIComponent(k)}=${encodeURIComponent((knobs[k].min/100).toString())}`,
-        `max_${encodeURIComponent(k)}=${encodeURIComponent((knobs[k].max/100).toString())}`,
-      ].join('&');
-    }).join('&');
-    const queryString = `seed_artists=${seedArtists.join(',')}&market=US&${knobsToQS}&limit=${limit}`;
+            return [
+              `min_${encodeURIComponent(k)}=${encodeURIComponent(
+                (knobs[k].min / 100).toString()
+              )}`,
+              `max_${encodeURIComponent(k)}=${encodeURIComponent(
+                (knobs[k].max / 100).toString()
+              )}`
+            ].join("&");
+          })
+          .join("&");
+    const queryString = `seed_artists=${seedArtists.join(
+      ","
+    )}&market=US&${knobsToQS}&limit=${limit}`;
     return await this.makeRequest(`recommendations?${queryString}`);
   }
 
@@ -86,29 +115,41 @@ export class ApiHelper {
       description: playlistData.description
     };
 
-    const createdPlaylist = await this.makeRequest(`me/playlists`, REQUEST_TYPE.POST, postBody);
+    const createdPlaylist = await this.makeRequest(
+      `me/playlists`,
+      REQUEST_TYPE.POST,
+      postBody
+    );
 
-    await this.makeRequest(`playlists/${createdPlaylist.id}/tracks`, REQUEST_TYPE.POST, {
-      uris: playlistData.tracks
-    });
+    await this.makeRequest(
+      `playlists/${createdPlaylist.id}/tracks`,
+      REQUEST_TYPE.POST,
+      {
+        uris: playlistData.tracks
+      }
+    );
 
     if (!playlistData.playNow) {
       return;
     }
 
-    return this.makeRequest('me/player/play', REQUEST_TYPE.PUT, {
+    return this.makeRequest("me/player/play", REQUEST_TYPE.PUT, {
       context_uri: createdPlaylist.uri
     });
   }
 
-  private async makeRequest(path: string, type: REQUEST_TYPE = REQUEST_TYPE.GET, postBody? : any) {
+  private async makeRequest(
+    path: string,
+    type: REQUEST_TYPE = REQUEST_TYPE.GET,
+    postBody?: any
+  ) {
     await this.refreshUserToken();
 
     const res = await fetch(`${ROOT_URL}/${path}`, {
       headers: {
-        Accept: '*/*',
+        Accept: "*/*",
         Authorization: `Bearer ${this.accessToken}`,
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       },
       method: type,
       body: postBody ? JSON.stringify(postBody) : undefined
@@ -129,14 +170,19 @@ export class ApiHelper {
     return json;
   }
 
-  private getHashParams(): {access_token?: string, refresh_token?: string, error? :string, expiresAt?: string} {
+  private getHashParams(): {
+    access_token?: string;
+    refresh_token?: string;
+    error?: string;
+    expiresAt?: string;
+  } {
     const hashParams = {} as any;
     let e: any;
     const r = /([^&;=]+)=?([^&;]*)/g;
     const q = self.location.hash.substring(1);
     // tslint:disable-next-line:no-conditional-assignment
-    while ( e = r.exec(q)) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
     }
     return hashParams;
   }
@@ -146,7 +192,9 @@ export class ApiHelper {
       return; // nothing to do
     }
 
-    const res = await fetch(`${AUTH_URL}/refresh-token?refresh_token=${this.refreshToken}`);
+    const res = await fetch(
+      `${AUTH_URL}/refresh-token?refresh_token=${this.refreshToken}`
+    );
 
     if (res.ok) {
       const body = await res.json();
@@ -155,7 +203,7 @@ export class ApiHelper {
       return this.accessToken;
     }
 
-    throw new Error('Failed to refresh token');
+    throw new Error("Failed to refresh token");
   }
 
   private updateTokensInStorage(accessToken: string, expiresAt: number) {
